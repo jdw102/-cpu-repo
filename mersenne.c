abd4ee7f71a339560e923484bd5c67c66eb9cc08
jdw102 <jerry.worthy@duke.edu>
2022-01-17 20:58:11 -0500

    adding old files

--- mersenne.c
+++ mersenne.c
@@ -5,7 +5,7 @@ int main(int argc, char* argv[]){
     int n = atoi(argv[1]);
     int ret = 1;
     for (int i = 0; i < n; i++){
-        ret = ret * 2;
+        ret = (ret * 2);
     }
     printf("%d\n", ret - 1);
     return ret - 1;
