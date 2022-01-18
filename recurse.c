93d5461918736100b1bf5c6116da33df7ae47a42
jdw102 <jerry.worthy@duke.edu>
2022-01-17 19:54:58 -0500

    Test

---
+++ recurse.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int function(int n){
+    if (n == 0){
+        return -2;
+    }
+    return 3 * n - ( 2 * f(n - 1)) - 2;
+}
+
+
+int main(int argc, char* argv[]){
+    int n = atoi(argv[1]);
+    int fn = function(n);
+    printf("%d", fn);
+    return fn;
+}
+
