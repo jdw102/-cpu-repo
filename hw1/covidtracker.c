66da1296bdb7d9a11d6cc941056d528352d28711
jdw102 <jerry.worthy@duke.edu>
2022-01-24 16:49:39 -0500

    Covidtracker

--- hw1/covidtracker.c
+++ hw1/covidtracker.c
@@ -1,60 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 struct person {
-    char name[30];
+    char* name[30];
     struct person* a[2];
     struct person* next; 
 };
-
-int main(int argc, char* argv[]){
-        char* filename = argv[1];
-        FILE *f1 = fopen(filename, "r");
-        if (f1 == NULL){
-            printf("ERROR: COULD NOT OPEN FILE");
-            return 1;
-        }
-        int size = 0;
-        struct person* final = NULL;
-        struct person* previous = NULL;
-        char c;
-        while (true){
-            char currname[30] = "";
-            for (c=getc(fp); c != EOF; c = getc(fp)){
-                if (c == ' ' || c == '\n'){
-                    break;
-                }
-                name += c;
-            }
-            if (strcmp(currname, "DONE") == 0){
-                final = previous;
-                break;
-            }
-            if (c == ' '){
-                if (search(currname) == true){
-                    continue
-                }
-                else{
-                    struct person* p = (struct person*) malloc(sizeof(struct person*));
-                    p->name = currname;
-                }
-            }
-            if (c == '\n'){
-                if (search(currname) == true){
-                    mutate(currname, previous->name);
-                }
-                else{
-                    struct person* p = (struct person*) malloc(sizeof(struct person*));
-                    p->name = currname;
-                    p->a[0] = previous->name;
-                }
-            }
-            p->next = previous;
-            previous = p;
-        }
-        printlist(final);
-        return 0;  
-}
 
 bool search(struct person* head, char* target[]){
     while (head != null){
@@ -84,11 +36,6 @@ void mutate(struct person* head, char* target[], char* victim[]){
         }
         head = head->next;
     }
-
-}
-
-void sortlist(struct person* head){
-
 }
 
 void printlist(struct person* head){
@@ -96,6 +43,60 @@ void printlist(struct person* head){
         printf("%s %s %s\n", head->name, head->a[0], head->a[1]);
         head = head->next;
     }
+}
 
+
+int main(int argc, char* argv[]){
+        char* filename = argv[1];
+        FILE *f1 = fopen(filename, "r");
+        if (f1 == NULL){
+            printf("ERROR: COULD NOT OPEN FILE");
+            return 1;
+        }
+        int size = 0;
+        struct person* final = NULL;
+        struct person* previous = NULL;
+        char c;
+        while (true){
+            char currname[30] = "";
+            for (c=getc(f1); c != EOF; c = getc(f1)){
+                if (c == ' ' || c == '\n'){
+                    break;
+                }
+                currname += c;
+            }
+            if (strcmp(currname, "DONE") == 0){
+                final = previous;
+                break;
+            }
+            if (c == ' '){
+                if (search(currname) == true){
+                    continue
+                }
+                else{
+                    struct person* p = (struct person*) malloc(sizeof(struct person*));
+                    strcpy(p->name, currname[0]);
+                }
+            }
+            if (c == '\n'){
+                if (search(currname) == true){
+                    mutate(currname, previous->name);
+                }
+                else{
+                    struct person* p = (struct person*) malloc(sizeof(struct person*));
+                    p->name = currname;
+                    p->a[0] = previous->name;
+                }
+            }
+            p->next = previous;
+            previous = p;
+        }
+        printlist(final);
+        return 0;  
+}
+
+
+
+void sortlist(struct person* head){
 
 }
